911647fb201345416de68e613b842e605fd66135
Domnigg20 <ariyibipeter69@gmail.com>
2023-03-24 04:34:42 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+//Main- A program that prints a line with puts 
+//Return = O (success)
+
+int main(){
+	puts("Programming is like building a multilingual puzzle, "\n");
+	return 0;
+}
