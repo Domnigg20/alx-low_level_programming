d07b11b03683a401f02490dacff589b8a2f8052f
Domnigg20 <ariyibipeter69@gmail.com>
2023-03-31 01:45:47 -0700

    0-strcat.c

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+/**
+ * _strcat - concatenates 2 strings.
+ * @dest: strng wth concatenatn
+ * @src: string to b concatenatd
+ * Return: Always 0.
+ */
+char *_strcat(char *dest, char *src)
+{
+	int lengthD, lengthS;
+
+	lengthD = 0;
+	lengthS = 0;
+
+	while (*(dest + lengthD) != '\0')
+		lengthD++;
+
+	while (*(src + lengthS) != '\0' && lengthD < 97)
+	{
+		*(dest + lengthD) = *(src + lengthS);
+		lengthD++;
+		lengthS++;
+	}
+	*(dest + lengthD) = '\0';
+	return (dest);
+}
+
