fe5d5be7393e647ef713e5715f6b4992e6df8ffa
root <you@example.com>
2023-05-26 18:21:41 -0700

    3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,16 +1,17 @@
 #include "main.h"
-
+#include "holberton.h"
 /**
- * print_numrss - prints d numbers, from 0 to 9,
- * followd by a new line
+ *print_numbers - print numbers 0-9
+ *
+ *Return:always 0
  */
+
 void print_numbers(void)
 {
-	int i;
-
-	for (i = 0; i < 10; i++)
-	{
-		_putchar(i + '0');
-	}
-
-	_putcha
+int num;
+for (num = 0; num <= 9; num++)
+{
+	_putchar(num + '0');
+}
+	_putchar('\n');
+}
